184ae4ab41d93590c7709b82d8ee76e6443e1ebd
Jasen Kennington <jasen@kennington.io>
2016-10-03 12:08:32 -0500

    Add framework targets for macOS, iOS, tvOS, and watchOS

---
+++ Sources/SwiftGraph.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGraph.h
+//  SwiftGraph
+//
+//  Created by Jasen Kennington on 10/3/16.
+//  Copyright © 2016 Oak Snow Consulting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftGraph.
+FOUNDATION_EXPORT double SwiftGraph_VersionNumber;
+
+//! Project version string for SwiftGraph.
+FOUNDATION_EXPORT const unsigned char SwiftGraph_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGraph/PublicHeader.h>
+
+
