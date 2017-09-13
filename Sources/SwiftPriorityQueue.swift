4df819a9ae921b4fd60174407b401275723fd2dd
Dave <snpukwana@gmail.com>
2017-09-12 23:16:20 -0400

    Update to Swift 4; Update Project to Xcode 9; Closes #25

--- Sources/SwiftPriorityQueue.swift
+++ Sources/SwiftPriorityQueue.swift
@@ -2,7 +2,7 @@
 //  SwiftPriorityQueue.swift
 //  SwiftPriorityQueue
 //
-//  Copyright (c) 2015 David Kopec
+//  Copyright (c) 2015-2017 David Kopec
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -35,12 +35,16 @@ public struct PriorityQueue<T: Comparable> {
     private let ordered: (T, T) -> Bool
     
     public init(ascending: Bool = false, startingValues: [T] = []) {
-        
-        if ascending {
-            ordered = { $0 > $1 }
-        } else {
-            ordered = { $0 < $1 }
-        }
+        self.init(order: ascending ? { $0 > $1 } : { $0 < $1 }, startingValues: startingValues)
+    }
+    
+    /// Creates a new PriorityQueue with the given ordering.
+    ///
+    /// - parameter order: A function that specifies whether its first argument should
+    ///                    come after the second argument in the PriorityQueue.
+    /// - parameter startingValues: An array of elements to initialize the PriorityQueue with.
+    public init(order: @escaping (T, T) -> Bool, startingValues: [T] = []) {
+        ordered = order
         
         // Based on "Heap construction" from Sedgewick p 323
         heap = startingValues
@@ -73,7 +77,7 @@ public struct PriorityQueue<T: Comparable> {
         if heap.isEmpty { return nil }
         if heap.count == 1 { return heap.removeFirst() }  // added for Swift 2 compatibility
         // so as not to call swap() with two instances of the same location
-        swap(&heap[0], &heap[heap.count - 1])
+        heap.swapAt(0, heap.count - 1)
         let temp = heap.removeLast()
         sink(0)
         
@@ -87,7 +91,7 @@ public struct PriorityQueue<T: Comparable> {
     /// - parameter item: The item to remove the first occurrence of.
     public mutating func remove(_ item: T) {
         if let index = heap.index(of: item) {
-            swap(&heap[index], &heap[heap.count - 1])
+            heap.swapAt(index, heap.count - 1)
             heap.removeLast()
             swim(index)
             sink(index)
@@ -116,11 +120,7 @@ public struct PriorityQueue<T: Comparable> {
     
     /// Eliminate all of the elements from the Priority Queue.
     public mutating func clear() {
-        #if swift(>=3.0)
-            heap.removeAll(keepingCapacity: false)
-        #else
-            heap.removeAll(keepCapacity: false)
-        #endif
+        heap.removeAll(keepingCapacity: false)
     }
     
     // Based on example from Sedgewick p 316
@@ -133,7 +133,7 @@ public struct PriorityQueue<T: Comparable> {
             if j < (heap.count - 1) && ordered(heap[j], heap[j + 1]) { j += 1 }
             if !ordered(heap[index], heap[j]) { break }
             
-            swap(&heap[index], &heap[j])
+            heap.swapAt(index, j)
             index = j
         }
     }
@@ -142,7 +142,7 @@ public struct PriorityQueue<T: Comparable> {
     private mutating func swim(_ index: Int) {
         var index = index
         while index > 0 && ordered(heap[(index - 1) / 2], heap[index]) {
-            swap(&heap[(index - 1) / 2], &heap[index])
+            heap.swapAt((index - 1) / 2, index)
             index = (index - 1) / 2
         }
     }
@@ -172,11 +172,9 @@ extension PriorityQueue: Collection {
     
     public subscript(i: Int) -> T { return heap[i] }
     
-    #if swift(>=3.0)
-        public func index(after i: PriorityQueue.Index) -> PriorityQueue.Index {
-            return heap.index(after: i)
-        }
-    #endif
+    public func index(after i: PriorityQueue.Index) -> PriorityQueue.Index {
+        return heap.index(after: i)
+    }
 }
 
 // MARK: - CustomStringConvertible, CustomDebugStringConvertible
@@ -185,3 +183,4 @@ extension PriorityQueue: CustomStringConvertible, CustomDebugStringConvertible {
     public var description: String { return heap.description }
     public var debugDescription: String { return heap.debugDescription }
 }
+
