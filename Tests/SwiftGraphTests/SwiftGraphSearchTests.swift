7633384aebd616e73591c41016bb543287ef50af
William Esz <flagaello@gmail.com>
2018-01-16 20:59:25 +0100

    Update tests to Dijkstra utilities changes

--- Tests/SwiftGraphTests/SwiftGraphSearchTests.swift
+++ Tests/SwiftGraphTests/SwiftGraphSearchTests.swift
@@ -16,16 +16,16 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-import XCTest
 @testable import SwiftGraph
+import XCTest
 
 class SwiftGraphSearchTests: XCTestCase {
     // pg 1016 Liang
     let cityGraph: UnweightedGraph<String> = UnweightedGraph<String>(vertices: ["Seattle", "San Francisco", "Los Angeles", "Denver", "Kansas City", "Chicago", "Boston", "New York", "Atlanta", "Miami", "Dallas", "Houston"])
-    
+
     // 15 largest MSAs in United States as of 2016
     let cityGraph2: UnweightedGraph<String> = UnweightedGraph<String>(vertices: ["Seattle", "San Francisco", "Los Angeles", "Riverside", "Phoenix", "Chicago", "Boston", "New York", "Atlanta", "Miami", "Dallas", "Houston", "Detroit", "Philadelphia", "Washington"])
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -54,7 +54,7 @@ class SwiftGraphSearchTests: XCTestCase {
         cityGraph.addEdge(from: "Houston", to: "Miami")
         cityGraph.addEdge(from: "Houston", to: "Dallas")
         print(cityGraph.description)
-        
+
         cityGraph2.addEdge(from: "Seattle", to: "Chicago")
         cityGraph2.addEdge(from: "Seattle", to: "San Francisco")
         cityGraph2.addEdge(from: "San Francisco", to: "Riverside")
@@ -83,12 +83,12 @@ class SwiftGraphSearchTests: XCTestCase {
         cityGraph2.addEdge(from: "Philadelphia", to: "Washington")
         print(cityGraph2.description)
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testDFS1() {
         // Seattle -> Miami
         let result = cityGraph.dfs(from: "Seattle", to: "Miami")
@@ -99,9 +99,9 @@ class SwiftGraphSearchTests: XCTestCase {
         if let first = result.first {
             XCTAssertEqual(cityGraph.vertexAtIndex(first.u), "Seattle", "Seattle is not the start")
         }
-        print(edgesToVertices(edges: result, graph: cityGraph))
+        print(cityGraph.edgesToVertices(edges: result))
     }
-    
+
     func testDFS2() {
         // Boston -> LA
         let result = cityGraph.dfs(from: "Boston", to: "Los Angeles")
@@ -112,13 +112,13 @@ class SwiftGraphSearchTests: XCTestCase {
         if let first = result.first {
             XCTAssertEqual(cityGraph.vertexAtIndex(first.u), "Boston", "Boston is not the start")
         }
-        print(edgesToVertices(edges: result, graph: cityGraph))
+        print(cityGraph.edgesToVertices(edges: result))
     }
-    
+
     func testDFS3() {
         // Houston -> first city starting with "N"
         let result = cityGraph2.dfs(from: "Houston") { v in
-            return v.characters.first == "N"
+            return v.first == "N"
         }
         XCTAssertFalse(result.isEmpty, "Couldn't find connection between Houston and a city starting with N (there is one).")
         if let last = result.last {
@@ -127,9 +127,9 @@ class SwiftGraphSearchTests: XCTestCase {
         if let first = result.first {
             XCTAssertEqual(cityGraph2.vertexAtIndex(first.u), "Houston", "Houston is not the start")
         }
-        print(edgesToVertices(edges: result, graph: cityGraph2))
+        print(cityGraph2.edgesToVertices(edges: result))
     }
-    
+
     func testDFS4() {
         // Seattle -> Miami
         let result = cityGraph2.dfs(from: "Seattle", to: "Miami")
@@ -140,9 +140,9 @@ class SwiftGraphSearchTests: XCTestCase {
         if let first = result.first {
             XCTAssertEqual(cityGraph2.vertexAtIndex(first.u), "Seattle", "Seattle is not the start")
         }
-        print(edgesToVertices(edges: result, graph: cityGraph2))
+        print(cityGraph2.edgesToVertices(edges: result))
     }
-    
+
     func testDFS5() {
         // Boston -> LA
         let result = cityGraph2.dfs(from: "Boston", to: "Los Angeles")
@@ -153,13 +153,13 @@ class SwiftGraphSearchTests: XCTestCase {
         if let first = result.first {
             XCTAssertEqual(cityGraph2.vertexAtIndex(first.u), "Boston", "Boston is not the start")
         }
-        print(edgesToVertices(edges: result, graph: cityGraph2))
+        print(cityGraph2.edgesToVertices(edges: result))
     }
-    
+
     func testDFS6() {
         // Houston -> first city starting with "N"
         let result = cityGraph2.dfs(from: "Houston") { v in
-            return v.characters.first == "N"
+            return v.first == "N"
         }
         XCTAssertFalse(result.isEmpty, "Couldn't find connection between Houston and a city starting with N (there is one).")
         if let last = result.last {
@@ -168,9 +168,9 @@ class SwiftGraphSearchTests: XCTestCase {
         if let first = result.first {
             XCTAssertEqual(cityGraph2.vertexAtIndex(first.u), "Houston", "Houston is not the start")
         }
-        print(edgesToVertices(edges: result, graph: cityGraph2))
+        print(cityGraph2.edgesToVertices(edges: result))
     }
-    
+
     func testBFS1() {
         // Seattle -> Miami
         let result = cityGraph.bfs(from: "Seattle", to: "Miami")
@@ -182,9 +182,9 @@ class SwiftGraphSearchTests: XCTestCase {
             XCTAssertEqual(cityGraph.vertexAtIndex(first.u), "Seattle", "Seattle is not the start")
         }
         XCTAssertEqual(result.count, 4, "Expect to take 4 edges to get from Seattle to Miami")
-        print(edgesToVertices(edges: result, graph: cityGraph))
+        print(cityGraph.edgesToVertices(edges: result))
     }
-    
+
     func testBFS2() {
         // Boston -> LA
         let result = cityGraph.bfs(from: "Boston", to: "Los Angeles")
@@ -196,13 +196,13 @@ class SwiftGraphSearchTests: XCTestCase {
             XCTAssertEqual(cityGraph.vertexAtIndex(first.u), "Boston", "Boston is not the start")
         }
         XCTAssertEqual(result.count, 3, "Expect to take 3 edges to get from Boston to Los Angeles")
-        print(edgesToVertices(edges: result, graph: cityGraph))
+        print(cityGraph.edgesToVertices(edges: result))
     }
-    
+
     func testBFS3() {
         // Houston -> first city starting with "N"
         let result = cityGraph.bfs(from: "Houston") { v in
-            return v.characters.first == "N"
+            return v.first == "N"
         }
         XCTAssertFalse(result.isEmpty, "Couldn't find connection between Houston and a city starting with N (there is one).")
         if let last = result.last {
@@ -211,9 +211,9 @@ class SwiftGraphSearchTests: XCTestCase {
         if let first = result.first {
             XCTAssertEqual(cityGraph.vertexAtIndex(first.u), "Houston", "Houston is not the start")
         }
-        print(edgesToVertices(edges: result, graph: cityGraph))
+        print(cityGraph.edgesToVertices(edges: result))
     }
-    
+
     func testBFS4() {
         // Seattle -> Miami
         let result = cityGraph2.bfs(from: "Seattle", to: "Miami")
@@ -225,9 +225,9 @@ class SwiftGraphSearchTests: XCTestCase {
             XCTAssertEqual(cityGraph2.vertexAtIndex(first.u), "Seattle", "Seattle is not the start")
         }
         XCTAssertEqual(result.count, 3, "Expect to take 3 edges to get from Seattle to Miami")
-        print(edgesToVertices(edges: result, graph: cityGraph2))
+        print(cityGraph2.edgesToVertices(edges: result))
     }
-    
+
     func testBFS5() {
         // Boston -> LA
         let result = cityGraph2.bfs(from: "Boston", to: "Los Angeles")
@@ -239,13 +239,13 @@ class SwiftGraphSearchTests: XCTestCase {
             XCTAssertEqual(cityGraph2.vertexAtIndex(first.u), "Boston", "Boston is not the start")
         }
         XCTAssertEqual(result.count, 4, "Expect to take 4 edges to get from Boston to Los Angeles")
-        print(edgesToVertices(edges: result, graph: cityGraph2))
+        print(cityGraph2.edgesToVertices(edges: result))
     }
-    
+
     func testBFS6() {
         // Houston -> first city starting with "N"
         let result = cityGraph2.bfs(from: "Houston") { v in
-            return v.characters.first == "N"
+            return v.first == "N"
         }
         XCTAssertFalse(result.isEmpty, "Couldn't find connection between Houston and a city starting with N (there is one).")
         if let last = result.last {
@@ -254,33 +254,33 @@ class SwiftGraphSearchTests: XCTestCase {
         if let first = result.first {
             XCTAssertEqual(cityGraph2.vertexAtIndex(first.u), "Houston", "Houston is not the start")
         }
-        print(edgesToVertices(edges: result, graph: cityGraph2))
+        print(cityGraph2.edgesToVertices(edges: result))
     }
-    
+
     func testFindAll() {
         // New York -> all cities starting with "S"
         let result = cityGraph.findAll(from: "New York") { v in
-            return v.characters.first == "S"
+            return v.first == "S"
         }
         XCTAssertFalse(result.isEmpty, "Couldn't find any connections between New York and a city starting with S (there is one).")
         XCTAssertEqual(result.count, 2, "Should be 2 cities found starting with S")
         if let last = result.last, let first = result.first {
-            let pathOne = edgesToVertices(edges: last, graph: cityGraph)
+            let pathOne = cityGraph.edgesToVertices(edges: last)
             print(pathOne)
-            let pathTwo = edgesToVertices(edges: first, graph: cityGraph)
+            let pathTwo = cityGraph.edgesToVertices(edges: first)
             print(pathTwo)
             let endCities = [pathOne.last!, pathTwo.last!]
             XCTAssertTrue(endCities.contains("Seattle"), "Should contain a route to Seattle")
             XCTAssertTrue(endCities.contains("San Francisco"), "Should contain a route to San Francisco")
         }
     }
-    
-    /*func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }*/
+
+    /* func testPerformanceExample() {
+     // This is an example of a performance test case.
+     self.measureBlock() {
+     // Put the code you want to measure the time of here.
+     }
+     } */
     static var allTests = [
         ("testDFS1", testDFS1),
         ("testDFS2", testDFS2),
@@ -294,6 +294,6 @@ class SwiftGraphSearchTests: XCTestCase {
         ("testBFS4", testBFS4),
         ("testBFS5", testBFS5),
         ("testBFS6", testBFS6),
-        ("testFindAll", testFindAll)
+        ("testFindAll", testFindAll),
     ]
 }
