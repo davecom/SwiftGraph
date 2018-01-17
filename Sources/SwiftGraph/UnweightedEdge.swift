e6620c6c2b355f147e92d8b17bc48a4407fcda9f
William Esz <flagaello@gmail.com>
2018-01-17 17:55:28 +0100

    Refactor UnweightedEdge to a protocol

--- Sources/SwiftGraph/UnweightedEdge.swift
+++ Sources/SwiftGraph/UnweightedEdge.swift
@@ -17,32 +17,28 @@
 //  limitations under the License.
 
 /// A basic unweighted edge.
-open class UnweightedEdge: Edge, Equatable, CustomStringConvertible {
-    public var u: Int
-    public var v: Int
-    public var weighted: Bool { return false }
-    public let directed: Bool
-    public var reversed: Edge {
-        return UnweightedEdge(u: v, v: u, directed: directed)
+protocol UnweightedEdge: Edge {
+    init(u: Int, v: Int, directed: Bool)
+}
+
+extension UnweightedEdge {
+    public var weighted: Bool {
+        return false
     }
 
-    public init(u: Int, v: Int, directed: Bool) {
-        self.u = u
-        self.v = v
-        self.directed = directed
+    public var reversed: Self {
+        return Self(u: v, v: u, directed: directed)
     }
+}
 
-    // Implement Printable protocol
-    public var description: String {
-        if directed {
-            return "\(u) -> \(v)"
-        }
-        return "\(u) <-> \(v)"
-    }
-
-    // MARK: Operator Overloads
-
-    public static func == (lhs: UnweightedEdge, rhs: UnweightedEdge) -> Bool {
+extension UnweightedEdge {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
         return lhs.u == rhs.u && lhs.v == rhs.v && lhs.directed == rhs.directed
+    }
+}
+
+extension UnweightedEdge {
+    public var description: String {
+        return directed ? "\(u) -> \(v)" : "\(u) <-> \(v)"
     }
 }
