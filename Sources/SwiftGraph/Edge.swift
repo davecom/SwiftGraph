c990bd12abd1136b691e438fdbec6787fff298dc
William Esz <flagaello@gmail.com>
2018-01-17 17:54:15 +0100

    Make Edge conform to Equatable

--- Sources/SwiftGraph/Edge.swift
+++ Sources/SwiftGraph/Edge.swift
@@ -17,7 +17,7 @@
 //  limitations under the License.
 
 /// A protocol that all edges in a graph must conform to.
-public protocol Edge: CustomStringConvertible {
+public protocol Edge: Equatable, CustomStringConvertible {
     /// The origin vertex of the edge
     var u: Int { get set } // made modifiable for changing when removing vertices
     /// The destination vertex of the edge
