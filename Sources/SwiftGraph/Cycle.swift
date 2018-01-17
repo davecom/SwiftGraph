bd62b287b0fe32c719b543b25993bed1e2d07b34
William Esz <flagaello@gmail.com>
2018-01-17 22:36:09 +0100

    Update cycles(until:) to Graph protocol

--- Sources/SwiftGraph/Cycle.swift
+++ Sources/SwiftGraph/Cycle.swift
@@ -16,11 +16,9 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-/// Functions for finding cycles in a `Graph`
+// MARK: Cycles
 
-// MARK: Extension to `Graph` for detecting cycles
-
-public extension Graph {
+extension Graph {
     // Based on an algorithm developed by Hongbo Liu and Jiaxin Wang
     // Liu, Hongbo, and Jiaxin Wang. "A new way to enumerate cycles in graph."
     // In Telecommunications, 2006. AICT-ICIW'06. International Conference on Internet and
@@ -28,20 +26,20 @@ public extension Graph {
 
     /// Find all of the cycles in a `Graph`, expressed as vertices.
     ///
-    /// - parameter upToLength: Does the caller only want to detect cycles up to a certain length?
+    /// - parameter length: Does the caller only want to detect cycles up to a certain length?
     /// - returns: a list of lists of vertices in cycles
-    public func detectCycles(upToLength maxK: Int = Int.max) -> [[V]] {
+    public func cycles(until length: Int = Int.max) -> [[V]] {
         var cycles = [[V]]() // store of all found cycles
         var openPaths: [[V]] = vertices.map { [$0] } // initial open paths are single vertex lists
 
         while !openPaths.isEmpty {
             let openPath = openPaths.removeFirst() // queue pop()
-            if openPath.count > maxK { return cycles } // do we want to stop at a certain length k
-            if let tail = openPath.last, let head = openPath.first, let neighbors = neighborsForVertex(tail) {
+            if openPath.count > length { return cycles } // do we want to stop at a certain length k
+            if let tail = openPath.last, let head = openPath.first, let neighbors = neighbors(for: tail) {
                 for neighbor in neighbors {
                     if neighbor == head {
                         cycles.append(openPath + [neighbor]) // found a cycle
-                    } else if !openPath.contains(neighbor) && indexOfVertex(neighbor)! > indexOfVertex(head)! {
+                    } else if !openPath.contains(neighbor) && index(of: neighbor)! > index(of: head)! {
                         openPaths.append(openPath + [neighbor]) // another open path to explore
                     }
                 }
@@ -53,19 +51,18 @@ public extension Graph {
 
     /// Find all of the cycles in a `Graph`, expressed as edges.
     ///
-    /// - parameter upToLength: Does the caller only want to detect cycles up to a certain length?
+    /// - parameter length: Does the caller only want to detect cycles up to a certain length?
     /// - returns: a list of lists of edges in cycles
-    public func detectCyclesAsEdges(upToLength maxK: Int = Int.max) -> [[Edge]] {
-
-        var cycles = [[Edge]]() // store of all found cycles
-        var openPaths: [Path] = (0 ..< vertices.count).map(Path.init(start:)) // initial open paths start at a vertex, and are empty
+    public func cycles(until length: Int = Int.max) -> [[E]] {
+        var cycles = [[E]]() // store of all found cycles
+        var openPaths: [Path<E>] = (0 ..< vertices.count).map(Path<E>.init(start:)) // initial open paths start at a vertex, and are empty
 
         while !openPaths.isEmpty {
             let openPath = openPaths.removeFirst() // queue pop()
-            if openPath.path.count > maxK { return cycles } // do we want to stop at a certain length k
+            if openPath.path.count > length { return cycles } // do we want to stop at a certain length
             let tail = openPath.tail
             let head = openPath.head
-            let neighborEdges = edgesForIndex(tail)
+            let neighborEdges = edges(for: tail)
             for neighborEdge in neighborEdges {
                 if neighborEdge.v == head {
                     cycles.append(openPath.path + [neighborEdge]) // found a cycle
@@ -79,28 +76,27 @@ public extension Graph {
     }
 }
 
-private extension Graph {
-    /// Data structure used exclusively in `detectCylesAsEdges()`
-    struct Path {
-        var start: Int
-        var path: [Edge] = []
+// MARK: Cycles Utilities
 
-        init(start: Int) {
-            self.start = start
-        }
+private struct Path<E: Edge> {
+    var start: Int
+    var path: [E] = []
 
-        func byAdding(_ edge: Edge) -> Path {
-            var mutable = self
-            mutable.path.append(edge)
-            return mutable
-        }
+    init(start: Int) {
+        self.start = start
+    }
 
-        var head: Int {
-            return start
-        }
+    func byAdding(_ edge: E) -> Path {
+        var mutable = self
+        mutable.path.append(edge)
+        return mutable
+    }
 
-        var tail: Int {
-            return path.last?.v ?? start
-        }
+    var head: Int {
+        return start
+    }
+
+    var tail: Int {
+        return path.last?.v ?? start
     }
 }
