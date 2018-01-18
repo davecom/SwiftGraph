bcd406955740fd5de5ab317b37d4daf99bf2c15d
William Esz <flagaello@gmail.com>
2018-01-18 17:22:23 +0100

    Disable SwiftLint type_name in Internals

--- Sources/SwiftGraph/Internals.swift
+++ Sources/SwiftGraph/Internals.swift
@@ -16,6 +16,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+// swiftlint:disable type_name
+
 internal final class _UnweightedEdge: UnweightedEdge {
     init(u: Int, v: Int, directed: Bool) {
         self.u = u
