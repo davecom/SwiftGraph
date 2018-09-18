1962a730fe8eb752fcfd2852ba7bbfbbc2648225
Dave <snpukwana@gmail.com>
2018-09-17 23:26:25 -0400

    Sample App fix for Swift 4.2

--- SwiftGraphSampleApp/AppDelegate.swift
+++ SwiftGraphSampleApp/AppDelegate.swift
@@ -31,7 +31,7 @@ class NineTailView: NSView {
                 for j in 0..<position.positionMatrix[0].count  {
                     CATransaction.begin()
                     CATransaction.setValue(NSNumber(value: 2.5), forKey: kCATransactionAnimationDuration)
-                    pennyLayers[i][j].contents = NSImage(named: NSImage.Name(rawValue: position.positionMatrix[i][j].rawValue))!
+                    pennyLayers[i][j].contents = NSImage(named: position.positionMatrix[i][j].rawValue)!
                     CATransaction.commit()
                 }
             }
@@ -51,7 +51,7 @@ class NineTailView: NSView {
         }
         for i in 0..<position.positionMatrix.count {
             for j in 0..<position.positionMatrix[0].count {
-                pennyLayers[i][j].contents = NSImage(named: NSImage.Name(rawValue: "heads"))
+                pennyLayers[i][j].contents = NSImage(named: "heads")
                 pennyLayers[i][j].frame = CGRect(x: CGFloat(CGFloat(i) * (width/3)), y: CGFloat(CGFloat(j) * (height/3)), width: (width/3), height: (height/3))
                 layer?.addSublayer(pennyLayers[i][j])
             }
