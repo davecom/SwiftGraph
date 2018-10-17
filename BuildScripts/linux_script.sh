7e657cd0d7f6ce5522138604fb933efbd6cffda7
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2018-10-17 19:04:59 +0200

    Disable performance tests in CI
    Until we have a way to set baselines in the CI server

--- BuildScripts/linux_script.sh
+++ BuildScripts/linux_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 swift test -c debug --filter "SwiftGraphTests"
-swift test -c release -Xswiftc -enable-testing --filter "SwiftGraphPerformanceTests"
+# swift test -c release -Xswiftc -enable-testing --filter "SwiftGraphPerformanceTests"
