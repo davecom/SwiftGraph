d4d13279323ec9c47e6a5156bcd82ffde09c91a2
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2018-10-18 21:33:45 +0200

    Add comment

--- Tests/SwiftGraphTests/Utils/EquatableTypes.swift
+++ Tests/SwiftGraphTests/Utils/EquatableTypes.swift
@@ -16,6 +16,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+/// A wrapper around String but dropping Hashable conformance
 struct EquatableString: ExpressibleByStringLiteral, Equatable {
     init(stringLiteral: String) {
         s = stringLiteral
