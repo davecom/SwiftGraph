ae036d3985b2acfd5eca18981f0ff116c61af695
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2019-03-08 01:45:29 +0100

    Make UniqueElementsGraph work with any edge

--- Sources/SwiftGraph/UniqueElementsGraph.swift
+++ Sources/SwiftGraph/UniqueElementsGraph.swift
@@ -16,10 +16,12 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+public typealias UnweightedUniqueElementsGraph<V: Equatable> = UniqueElementsGraph<V, UnweightedEdge>
+
 /// A subclass of UnweightedGraph that ensures there are no pairs of equal vertices and no repeated edges.
-open class UniqueElementsGraph<V: Equatable>: Graph {
+open class UniqueElementsGraph<V: Equatable, E: Edge&Equatable>: Graph {
     public var vertices: [V] = [V]()
-    public var edges: [[UnweightedEdge]] = [[UnweightedEdge]]() //adjacency lists
+    public var edges: [[E]] = [[E]]() //adjacency lists
 
     public init() {
     }
@@ -44,29 +46,65 @@ open class UniqueElementsGraph<V: Equatable>: Graph {
         return vertices.count - 1
     }
 
-    /// Only allow the edge to be added once
+    /// Add an edge to the graph. Only allow the edge to be added once
     ///
     /// - parameter e: The edge to add.
-    public func addEdge(_ e: UnweightedEdge) {
-        if !edgeExists(from: e.u, to: e.v) {
+    /// - parameter directed: If false, undirected edges are created.
+    ///                       If true, a reversed edge is also created.
+    ///                       Default is false.
+    public func addEdge(_ e: E, directed: Bool = false) {
+        if !self.edgeExists(e) {
             edges[e.u].append(e)
         }
+        if !directed {
+            let reversedEdge = e.reversed()
+            if !edgeExists(reversedEdge) {
+                edges[e.v].append(reversedEdge)
+            }
+        }
     }
-    
+}
+
+extension UniqueElementsGraph where E == UnweightedEdge {
+
+    private func addEdgesForPath(withIndices indices: [Int], directed: Bool) {
+        for i in 0..<indices.count - 1 {
+            addEdge(fromIndex: indices[i], toIndex: indices[i+1], directed: directed)
+        }
+    }
+
+    /// Is there an edge from one vertex to another?
+    ///
+    /// - parameter from: The index of the starting edge.
+    /// - parameter to: The index of the ending edge.
+    /// - returns: A Bool that is true if such an edge exists, and false otherwise.
+    public func edgeExists(from: Int, to: Int) -> Bool {
+        return edgeExists(E(u: from, v: to))
+    }
+
+    /// Is there an edge from one vertex to another? Note this will look at the first occurence of each vertex. Also returns false if either of the supplied vertices cannot be found in the graph.
+    ///
+    /// - parameter from: The first vertex.
+    /// - parameter to: The second vertex.
+    /// - returns: A Bool that is true if such an edge exists, and false otherwise.
+    public func edgeExists(from: V, to: V) -> Bool {
+        if let u = indexOfVertex(from) {
+            if let v = indexOfVertex(to) {
+                return edgeExists(from: u, to: v)
+            }
+        }
+        return false
+    }
+
     /// Only allow the edge to be added once
     ///
     /// - parameter from: The starting vertex's index.
     /// - parameter to: The ending vertex's index.
     /// - parameter directed: Is the edge directed? (default `false`)
     public func addEdge(fromIndex u: Int, toIndex v: Int, directed: Bool = false) {
-        if !edgeExists(from: u, to: v) {
-            addEdge(UnweightedEdge(u: u, v: v))
-            if !directed && !edgeExists(from: v, to: u) {
-                addEdge(UnweightedEdge(u: v, v: u))
-            }
-        }
+        addEdge(UnweightedEdge(u: u, v: v), directed: directed)
     }
-    
+
     /// Only allow the edge to be added once
     ///
     /// - parameter from: The starting vertex.
@@ -75,15 +113,6 @@ open class UniqueElementsGraph<V: Equatable>: Graph {
     public func addEdge(from: V, to: V, directed: Bool = false) {
         if let u = indexOfVertex(from), let v = indexOfVertex(to) {
             addEdge(fromIndex: u, toIndex: v, directed: directed)
-        }
-    }
-}
-
-extension UniqueElementsGraph {
-
-    private func addEdgesForPath(withIndices indices: [Int], directed: Bool) {
-        for i in 0..<indices.count - 1 {
-            addEdge(fromIndex: indices[i], toIndex: indices[i+1], directed: directed)
         }
     }
 
@@ -104,9 +133,6 @@ extension UniqueElementsGraph {
         self.init(vertices: path)
 
         guard path.count >= 2 else {
-            if let v = path.first {
-                _ = addVertex(v)
-            }
             return
         }
 
@@ -148,7 +174,7 @@ extension UniqueElementsGraph {
 
 }
 
-extension UniqueElementsGraph where V: Hashable {
+extension UniqueElementsGraph where V: Hashable, E == UnweightedEdge {
     public convenience init(withPath path: [V], directed: Bool = false) {
         self.init()
 
